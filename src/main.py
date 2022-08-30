4c3828c26622827872e918ad3862c82235493a2d
yvanvrela <varelaivan30@gmail.com>
2022-08-29 23:20:15 -0400

    Delete home route

--- src/main.py
+++ src/main.py
@@ -1,20 +1,8 @@
+# FastApi
 from application import create_app
 import uvicorn
 
 app = create_app()
-
-
-
-
-@app.get(path='/', tags=['Home'], summary='Home in the app')
-async def home():
-    """Home
-
-    Returns:
-
-        json: json hello twitter
-    """
-    return {'Twitter': True}
 
 
 def main() -> None:
