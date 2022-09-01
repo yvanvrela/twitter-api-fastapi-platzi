81ca4c7cae4f4993246e2c731bd76b53bee32c84
yvanvrela <varelaivan30@gmail.com>
2022-09-01 11:38:12 -0400

    Add create tables

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # FastApi
 from application import create_app
+from database.scripts.create_tables import create_tables
 import uvicorn
 
 app = create_app()
@@ -8,6 +9,7 @@ app = create_app()
 def main() -> None:
     """Run App
     """
+    create_tables()
     uvicorn.run(
         app='main:app',
         host='127.0.0.1',
